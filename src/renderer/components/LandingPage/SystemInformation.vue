2ae432115dbd71e95d462327c5100a095165e800
Étienne Loiselle <loiselle.etienne@gmail.com>
2018-07-17 10:59:57 -0400

    Initial commit

---
+++ src/renderer/components/LandingPage/SystemInformation.vue
@@ -0,0 +1,58 @@
+<template>
+  <ul v-if="posts && posts.length">
+    <li v-for="post of posts">
+      <p><strong>{{post.title}}</strong></p>
+      <p>{{post.body}}</p>
+    </li>
+  </ul>
+</template>
+
+<script>
+  import axios from 'axios'
+
+  export default {
+    data () {
+      return {
+        posts: [],
+        errors: []
+      }
+    },
+    created () {
+      axios.get(`http://jsonplaceholder.typicode.com/posts`)
+        .then(response => {
+          // JSON responses are automatically parsed.
+          this.posts = response.data
+        })
+        .catch(e => {
+          // this.errors.push(e)
+        })
+    }
+}
+</script>
+
+<style scoped>
+  .title {
+    color: #888;
+    font-size: 18px;
+    font-weight: initial;
+    letter-spacing: .25px;
+    margin-top: 10px;
+  }
+
+  .items { margin-top: 8px; }
+
+  .item {
+    display: flex;
+    margin-bottom: 6px;
+  }
+
+  .item .name {
+    color: #6a6a6a;
+    margin-right: 6px;
+  }
+
+  .item .value {
+    color: #35495e;
+    font-weight: bold;
+  }
+</style>
